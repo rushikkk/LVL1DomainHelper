243d0364522b6c3029c67203f38064247351ab9c
Aleksey Golovnitsky <rufljke@gmail.com>
2018-04-24 12:33:20 +0300

    Alpha

--- LVL1DomainHelper/Form1.Designer.cs
+++ LVL1DomainHelper/Form1.Designer.cs
@@ -37,94 +37,117 @@
             this.mxRecord = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.nsRecords = new System.Windows.Forms.RichTextBox();
+            this.changeBackground = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // domainNameLabel
             // 
             this.domainNameLabel.AutoSize = true;
-            this.domainNameLabel.Location = new System.Drawing.Point(52, 42);
+            this.domainNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.domainNameLabel.Location = new System.Drawing.Point(50, 30);
             this.domainNameLabel.Name = "domainNameLabel";
-            this.domainNameLabel.Size = new System.Drawing.Size(69, 13);
+            this.domainNameLabel.Size = new System.Drawing.Size(87, 26);
             this.domainNameLabel.TabIndex = 0;
-            this.domainNameLabel.Text = "domainName";
+            this.domainNameLabel.Text = "Домен:";
+            this.domainNameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // domainName
             // 
+            this.domainName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.domainName.Location = new System.Drawing.Point(55, 59);
             this.domainName.Name = "domainName";
-            this.domainName.Size = new System.Drawing.Size(100, 20);
+            this.domainName.Size = new System.Drawing.Size(284, 26);
             this.domainName.TabIndex = 1;
             // 
             // buttonStart
             // 
-            this.buttonStart.Location = new System.Drawing.Point(281, 59);
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStart.Location = new System.Drawing.Point(366, 59);
             this.buttonStart.Name = "buttonStart";
-            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.Size = new System.Drawing.Size(149, 60);
             this.buttonStart.TabIndex = 2;
-            this.buttonStart.Text = "buttonStart";
+            this.buttonStart.Text = "Определить";
             this.buttonStart.UseVisualStyleBackColor = true;
             this.buttonStart.Click += new System.EventHandler(this.ButtonStart_Click);
             // 
             // ipAddressLabel
             // 
             this.ipAddressLabel.AutoSize = true;
-            this.ipAddressLabel.Location = new System.Drawing.Point(57, 118);
+            this.ipAddressLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ipAddressLabel.Location = new System.Drawing.Point(50, 93);
             this.ipAddressLabel.Name = "ipAddressLabel";
-            this.ipAddressLabel.Size = new System.Drawing.Size(72, 13);
+            this.ipAddressLabel.Size = new System.Drawing.Size(106, 26);
             this.ipAddressLabel.TabIndex = 3;
-            this.ipAddressLabel.Text = "IP-ADDRESS";
+            this.ipAddressLabel.Text = "IP-адрес:";
             // 
             // ipAddress
             // 
             this.ipAddress.BackColor = System.Drawing.Color.White;
-            this.ipAddress.Location = new System.Drawing.Point(60, 144);
+            this.ipAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ipAddress.Location = new System.Drawing.Point(55, 122);
             this.ipAddress.Name = "ipAddress";
             this.ipAddress.ReadOnly = true;
-            this.ipAddress.Size = new System.Drawing.Size(223, 20);
+            this.ipAddress.Size = new System.Drawing.Size(284, 26);
             this.ipAddress.TabIndex = 4;
             // 
             // mxRecordLabel
             // 
             this.mxRecordLabel.AutoSize = true;
-            this.mxRecordLabel.Location = new System.Drawing.Point(60, 186);
+            this.mxRecordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.mxRecordLabel.Location = new System.Drawing.Point(50, 160);
             this.mxRecordLabel.Name = "mxRecordLabel";
-            this.mxRecordLabel.Size = new System.Drawing.Size(61, 13);
+            this.mxRecordLabel.Size = new System.Drawing.Size(210, 26);
             this.mxRecordLabel.TabIndex = 5;
-            this.mxRecordLabel.Text = "MX-Record";
+            this.mxRecordLabel.Text = "Почта (MX-запись):";
             // 
             // mxRecord
             // 
             this.mxRecord.BackColor = System.Drawing.Color.White;
-            this.mxRecord.Location = new System.Drawing.Point(60, 203);
+            this.mxRecord.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.mxRecord.Location = new System.Drawing.Point(55, 189);
             this.mxRecord.Name = "mxRecord";
             this.mxRecord.ReadOnly = true;
-            this.mxRecord.Size = new System.Drawing.Size(245, 20);
+            this.mxRecord.Size = new System.Drawing.Size(284, 26);
             this.mxRecord.TabIndex = 6;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(60, 274);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(52, 231);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.Size = new System.Drawing.Size(287, 26);
             this.label1.TabIndex = 8;
-            this.label1.Text = "NS Records";
+            this.label1.Text = "DNS-серверы (NS-записи):";
             // 
             // nsRecords
             // 
             this.nsRecords.BackColor = System.Drawing.Color.White;
-            this.nsRecords.Location = new System.Drawing.Point(63, 291);
+            this.nsRecords.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nsRecords.Location = new System.Drawing.Point(55, 260);
             this.nsRecords.Name = "nsRecords";
             this.nsRecords.ReadOnly = true;
-            this.nsRecords.Size = new System.Drawing.Size(254, 96);
+            this.nsRecords.Size = new System.Drawing.Size(284, 96);
             this.nsRecords.TabIndex = 9;
             this.nsRecords.Text = "";
+            // 
+            // changeBackground
+            // 
+            this.changeBackground.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.changeBackground.Location = new System.Drawing.Point(366, 124);
+            this.changeBackground.Name = "changeBackground";
+            this.changeBackground.Size = new System.Drawing.Size(149, 62);
+            this.changeBackground.TabIndex = 10;
+            this.changeBackground.Text = "Сделать красиво";
+            this.changeBackground.UseVisualStyleBackColor = true;
+            this.changeBackground.Click += new System.EventHandler(this.changeBackground_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(537, 418);
+            this.Controls.Add(this.changeBackground);
             this.Controls.Add(this.nsRecords);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.mxRecord);
@@ -135,7 +158,7 @@
             this.Controls.Add(this.domainName);
             this.Controls.Add(this.domainNameLabel);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "DAPAMOGA";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -153,6 +176,7 @@
         private System.Windows.Forms.TextBox mxRecord;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.RichTextBox nsRecords;
+        private System.Windows.Forms.Button changeBackground;
     }
 }
 
--- LVL1DomainHelper/Form1.cs
+++ LVL1DomainHelper/Form1.cs
@@ -15,9 +15,14 @@ namespace LVL1DomainHelper
 
     public partial class Form1 : Form
     {
+        // Массив с IP-адресами нашего сервера
         String[] okby = new String[6] { "93.84.119.238", "93.84.119.239", "93.84.119.240", "93.84.119.241", "93.84.119.242", "93.84.119.243" };
+
+        //Инициализация объекта Matcher для диапазонов IP hoster.by
         Matcher hosterby = new Matcher();
-        
+
+        int bgclr = 0;
+
 
 
         public Form1()
@@ -27,55 +32,136 @@ namespace LVL1DomainHelper
 
         private void ButtonStart_Click(object sender, EventArgs e)
         {
-            System.Net.IPAddress[] ip = System.Net.Dns.GetHostAddresses(domainName.Text);
-            if (okby.Contains(ip[0].ToString()))
+            try
             {
-                ipAddress.Text = ip[0].ToString() + " (наш хостинг)";
-            }
-            else if (hosterby.MatchExists(ip[0].ToString()))
-            {
-                ipAddress.Text = ip[0].ToString() + " (hoster.by)";
-            }
-            else 
-            {
-                ipAddress.Text = ip[0].ToString();
-            }            
+                // Резолв имени домена
+                System.Net.IPAddress[] ip = System.Net.Dns.GetHostAddresses(domainName.Text);
 
-            var resolver = new DnsStubResolver();
+                // Определение вхождения IP-адреса в подсеть и вывод информации в поле 'IP-адрес'
+                if (okby.Contains(ip[0].ToString()))
+                {
+                    ipAddress.Text = ip[0].ToString() + " (наш хостинг)";
+                }
+                else if (hosterby.MatchExists(ip[0].ToString()))
+                {
+                    ipAddress.Text = ip[0].ToString() + " (hoster.by)";
+                }
+                else
+                {
+                    ipAddress.Text = ip[0].ToString();
+                }
 
-            var mxs = resolver.Resolve<MxRecord>(domainName.Text, RecordType.Mx);
-            String s1 = mxs[0].ToString();
-            String[] words = s1.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                // инициализация резолвера ресурсных записей домена
+                var resolver = new DnsStubResolver();
 
-            System.Net.IPAddress[] mxip = System.Net.Dns.GetHostAddresses(words.Last().TrimEnd('.'));
-            if (okby.Contains(mxip[0].ToString()))
+                // определение MX-записи домена
+                var mxs = resolver.Resolve<MxRecord>(domainName.Text, RecordType.Mx);
+                String s1 = mxs[0].ToString();
+                String[] words = s1.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                // Резолв IP-адреса MX-записи
+                System.Net.IPAddress[] mxip = System.Net.Dns.GetHostAddresses(words.Last().TrimEnd('.'));
+
+                // Определение вхождения IP-адреса MX-записи в подсеть и вывод информации в поле 'Почта (MX-запись)'
+                if (okby.Contains(mxip[0].ToString()))
+                {
+                    mxRecord.Text = words.Last().TrimEnd('.') + " (наш хостинг)";
+                }
+                else if (hosterby.MatchExists(mxip[0].ToString()))
+                {
+                    mxRecord.Text = words.Last().TrimEnd('.') + " hoster.by";
+                }
+                else
+                {
+                    mxRecord.Text = words.Last().TrimEnd('.');
+                }
+
+                // определение NS-записей домена
+                var nss = resolver.Resolve<NsRecord>(domainName.Text, RecordType.Ns);
+
+                // вывод NS-записей домена в поле 'DNS-серверы (NS-записи)'
+                nsRecords.Clear();
+                nss.Sort();
+                foreach (NsRecord element in nss)
+                {
+                    s1 = element.ToString();
+                    String[] wns = s1.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    nsRecords.Text += wns.Last().TrimEnd('.') + '\n';
+                }
+            }
+            catch (ArgumentNullException)
             {
-                mxRecord.Text = words.Last().TrimEnd('.') + " (наш хостинг)";
+                ipAddress.Clear();
+                mxRecord.Clear();
+                nsRecords.Clear();
+                MessageBox.Show("Введено неверное имя домена или данный домен не существует");
             }
-            else if (hosterby.MatchExists(mxip[0].ToString()))  {
-                mxRecord.Text = words.Last().TrimEnd('.') + " hoster.by";
-            }
-            else
+            catch (ArgumentOutOfRangeException)
             {
-                mxRecord.Text = words.Last().TrimEnd('.');
+                ipAddress.Clear();
+                mxRecord.Clear();
+                nsRecords.Clear();
+                MessageBox.Show("Введено неверное имя домена или данный домен не существует");
+            }
+            catch (ArgumentException)
+            {
+                ipAddress.Clear();
+                mxRecord.Clear();
+                nsRecords.Clear();
+                MessageBox.Show("Введено неверное имя домена или данный домен не существует");
+            }
+            catch (System.Net.Sockets.SocketException)
+            {
+                ipAddress.Clear();
+                mxRecord.Clear();
+                nsRecords.Clear();
+                MessageBox.Show("Введено неверное имя домена или данный домен не существует");
+            }
+            catch (Exception ex)
+            {
+                if (domainName.Text == "")
+                {
+                    ipAddress.Clear();
+                    mxRecord.Clear();
+                    nsRecords.Clear();
+                    MessageBox.Show("Введено неверное имя домена или данный домен не существует");
+                }
+                else
+                {
+                    MessageBox.Show(ex.Message);
+                }
             }
 
-            var nss = resolver.Resolve<NsRecord>(domainName.Text, RecordType.Ns);
-
-            nsRecords.Clear();
-            nss.Sort();
-            foreach (NsRecord element in nss)
-            {
-                s1 = element.ToString();
-                String[] wns = s1.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);                
-                nsRecords.Text += wns.Last().TrimEnd('.') + '\n';
-            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             domainName.Text = "2kakadu.by";
+            hosterby.Add("178.172.136.0", "255.255.248.0");
+            hosterby.Add("178.172.160.0", "255.255.252.0");
+            hosterby.Add("178.172.201.0", "255.255.255.0");
+            hosterby.Add("178.172.244.0", "255.255.255.0");
+            hosterby.Add("178.172.250.0", "255.255.255.0");
+            hosterby.Add("195.137.160.0", "255.255.255.0");
             hosterby.Add("93.125.99.0", "255.255.255.0");
+            Console.WriteLine(Form1.DefaultBackColor.IsNamedColor);
+        }
+
+        private void changeBackground_Click(object sender, EventArgs e)
+        {
+            if (bgclr == 0)
+            {
+                this.BackColor = Color.Pink;
+                changeBackground.Text = "Сделать нормально";
+                bgclr = 1;
+            }
+            else
+            {
+                this.BackColor = SystemColors.Control;
+                changeBackground.Text = "Сделать красиво";
+                bgclr = 0;
+            }
+            
         }
     }
 }
